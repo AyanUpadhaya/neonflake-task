f46cc3bc893349833f02afc9f5f707a0e567c7fb
Ayan Upadhaya <ayanu881@gmail.com>
2023-08-24 16:21:34 +0600

    first commit

---
+++ client/neonflak-client/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+function App() {
+ 
+  return (
+    <>
+      <div className="app">
+        <h1>Hello React!</h1>
+        <button className="button">
+          Action
+        </button>
+      </div>
+    </>
+  )
+}
+
+export default App
